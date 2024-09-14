34af72c3835e214e709cceafe0fab0ab500622df
Hinatazaka Campaign/日向坂キャンペーン中 <hinatazakacampaign@gmail.com>
2024-09-14 21:28:34 +0700

    fix: use DOMContentLoaded

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
-if (self === parent) {
-  document.querySelector("body").classList.add("self");
-} else {
-  document.querySelector("body").classList.add("child");
-}
+addEventListener("DOMContentLoaded", (event) => {
+  if (self === parent) {
+    document.querySelector("body").classList.add("self");
+  } else {
+    document.querySelector("body").classList.add("child");
+  }
 
-const playerEl = document.querySelector('#vid1');
-const player = videojs.getPlayer(playerEl);
+  const playerEl = document.querySelector('#vid1');
+  const player = videojs.getPlayer(playerEl);
 
-if (player) {
-  player.on('play', function () {
-    playerEl.removeChild(document.querySelector('.vjs-poster'));
-  });
-}
+  if (player) {
+    player.addEventListener('play', function () {
+      playerEl.removeChild(document.querySelector('.vjs-poster'));
+    });
+  }
 
-document.querySelectorAll(".hide-first").forEach(e => e.classList.remove('hide-first'));
+  document.querySelectorAll(".hide-first").forEach(e => e.classList.remove('hide-first'));
+});
