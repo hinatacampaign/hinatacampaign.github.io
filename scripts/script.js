74f8a675089f4d056ee306cbcd66983d24442fb0
Hinatazaka Campaign/日向坂キャンペーン中 <hinatazakacampaign@gmail.com>
2024-09-15 00:44:58 +0700

    fix: double listener

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector("body").classList.add("child");
   }
 
-  document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM loaded");
-    setTimeout(() => {
-      document
-        .querySelectorAll(
-          ".vjs-poster, .vjs-loading-spinner, .vjs-big-play-button"
-        )
-        .forEach((e) => e.remove());
-    }, 2000);
-  });
+  console.log("DOM loaded");
+  setTimeout(() => {
+    document
+      .querySelectorAll(
+        ".vjs-poster, .vjs-loading-spinner, .vjs-big-play-button"
+      )
+      .forEach((e) => e.remove());
+  }, 2000);
 
   document.querySelectorAll(".hide-first").forEach(e => e.classList.remove('hide-first'));
 });
